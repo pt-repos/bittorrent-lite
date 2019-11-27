3d6a5e3fe14fa0bfb564164232ed74270ca23594
Pulkit T <p.tripathi@ufl.edu>
2019-11-27 18:46:14 -0500

    created PeerInit class to intitate Peer process

--- src/bittorrent/peer/Peer.java
+++ src/bittorrent/peer/Peer.java
@@ -1,27 +1,41 @@
 package bittorrent.peer;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Peer {
 
-    public static void main(String[] args) throws Exception {
+    private int peerId;
+    private int fileOwnerPort;
+    private int downloadPeerPort;
 
-        if (args.length != 3) {
-            throw new Exception("Invalid Number of Arguments");
-        }
-        // TODO: 11/24/2019 Implement threaded listener for accepting connections as a server
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
+    public Peer(int peerId, int fileOwnerPort, int downloadPeerPort) {
+        this.peerId = peerId;
+        this.fileOwnerPort = fileOwnerPort;
+        this.downloadPeerPort = downloadPeerPort;
+    }
 
+    public void setPeerId(int peerId) {
+        this.peerId = peerId;
+    }
+
+    public void setFileOwnerPort(int fileOwnerPort) {
+        this.fileOwnerPort = fileOwnerPort;
+    }
+
+    public void setDownloadPeerPort(int downloadPeerPort) {
+        this.downloadPeerPort = downloadPeerPort;
+    }
+
+    public void start() throws IOException {
         try {
-            int peerId = Integer.parseInt(args[1]);
-
+            ExecutorService executorService = Executors.newFixedThreadPool(5);
             ServerSocket listener = new ServerSocket(peerId);
             executorService.submit(new ConnectionListener(listener));
-            
+
             String host = "localhost";
-            int fileOwnerPort = Integer.parseInt(args[0]);
             System.out.println("Trying to connect to FileOwner @ [" + fileOwnerPort + "]");
             try {
                 Thread peerAsClientToFileOwner = new Thread(new ClientProcess(host, fileOwnerPort, peerId));
@@ -30,7 +44,6 @@ public class Peer {
                 e.printStackTrace();
             }
 
-            int downloadPeerPort = Integer.parseInt(args[2]);
             System.out.println("Trying to connect to Download Neighbor @ [" + downloadPeerPort + "]");
             try {
                 Thread peerAsClientToPeer = new Thread(new ClientProcess(host, downloadPeerPort, peerId));
@@ -38,8 +51,38 @@ public class Peer {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+        } catch (IOException e) {
+            throw e;
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+//    public static void main(String[] args) throws Exception {
+//        // TODO: 11/24/2019 Implement threaded listener for accepting connections as a server
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//
+//        try {
+//            ServerSocket listener = new ServerSocket(peerId);
+//            executorService.submit(new ConnectionListener(listener));
+//
+//            String host = "localhost";
+//            System.out.println("Trying to connect to FileOwner @ [" + fileOwnerPort + "]");
+//            try {
+//                Thread peerAsClientToFileOwner = new Thread(new ClientProcess(host, fileOwnerPort, peerId));
+//                executorService.submit(peerAsClientToFileOwner);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("Trying to connect to Download Neighbor @ [" + downloadPeerPort + "]");
+//            try {
+//                Thread peerAsClientToPeer = new Thread(new ClientProcess(host, downloadPeerPort, peerId));
+//                executorService.submit(peerAsClientToPeer);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
