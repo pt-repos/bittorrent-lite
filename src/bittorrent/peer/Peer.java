c0ca91150c5a41257aebfcf0d470cecdf3fb03e1
Pulkit T <p.tripathi@ufl.edu>
2019-11-27 22:37:05 -0500

    refactor

--- src/bittorrent/peer/Peer.java
+++ src/bittorrent/peer/Peer.java
@@ -57,4 +57,23 @@ public class Peer {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            throw new Exception("Invalid Number of Arguments");
+        }
+
+        try {
+            int fileOwnerPort = Integer.parseInt(args[0]);
+            int peerId = Integer.parseInt(args[1]);
+            int downloadPeerPort = Integer.parseInt(args[2]);
+
+            Peer peer = new Peer(peerId, fileOwnerPort, downloadPeerPort);
+            peer.start();
+        } catch(NumberFormatException e) {
+            System.out.println("Invalid arguments");
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+    }
 }
